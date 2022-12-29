2f84c04883b6cff5d9a5a762ca420acc52177f3a
maksimvlasov <maks.mephi@gmail.com>
2022-12-29 21:09:11 +0300

    Add empty web project

---
+++ src/main/java/com/example/springboot3webclientsyncexample/SpringBoot3WebclientSyncExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot3webclientsyncexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3WebclientSyncExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3WebclientSyncExampleApplication.class, args);
+	}
+
+}
